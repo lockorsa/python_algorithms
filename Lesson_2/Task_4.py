3f0f122b7b34e415460bf08c7039d502a8708323
lockorsa <lockorsa@yandex.ru>
2021-05-18 22:43:26 +0700

    Tasks done

--- Lesson_2/Task_4.py
+++ Lesson_2/Task_4.py
@@ -2,13 +2,12 @@
 Найти сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ...Количество элементов (n) вводится с клавиатуры.
 """
 
-def func(n, tmp = 0, res = 1):
+def func(n, result = 1):
     if n == 0:
-        return n / -2
+        return 0
     else:
-        tmp = func(n - 1) / -2
-        res += tmp
-        return res
+        result += func(n - 1) / -2
+        return result
 
 
 n = int(input('Введите число: '))
