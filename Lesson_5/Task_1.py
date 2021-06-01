0d2abc6061944a6aed4a56868f0c430f3ae951b7
lockorsa <lockorsa@yandex.ru>
2021-06-01 21:31:50 +0700

    Task 2 done

--- Lesson_5/Task_1.py
+++ Lesson_5/Task_1.py
@@ -13,15 +13,10 @@
 from collections import namedtuple
 
 
-Company = namedtuple('Company', 'quarters_income, avg_income')
-
-
 def task_1(count_of_companies: int):
-
+    Company = namedtuple('Company', 'name, quarters_income, avg_income')
+    companies = []
     names = []
-    companies = {}
-    companies_above_avg = []
-    companies_below_avg = []
 
     # запросим названия компаний
     print('Введите названия компаний')
@@ -33,17 +28,19 @@ def task_1(count_of_companies: int):
         income = []
         for quarter in range(1, 5):
             income.append(int(input(f'Введите прибыль компании {name} за {quarter}-й квартал: ')))
-        companies[name] = Company(quarters_income=income, avg_income=sum(income))
+        companies.append(Company(name=name, quarters_income=income, avg_income=sum(income)))
 
     # считаем средний годовой доход всех компаний
-    avg_income_of_all = sum(value.avg_income for value in companies.values()) // len(companies)
+    avg_income_of_all = sum(company.avg_income for company in companies) // len(companies)
 
     # отсортируем компании с доходом выше/ниже среднего
-    for name, values in companies.items():
-        if values.avg_income > avg_income_of_all:
-            companies_above_avg.append(name)
-        elif values.avg_income < avg_income_of_all:
-            companies_below_avg.append(name)
+    companies_above_avg = []
+    companies_below_avg = []
+    for company in companies:
+        if company.avg_income > avg_income_of_all:
+            companies_above_avg.append(company)
+        elif company.avg_income < avg_income_of_all:
+            companies_below_avg.append(company)
 
     # вернем кортеж со средним доходом и словари с доходами компаний
     return avg_income_of_all, companies_above_avg, companies_below_avg
@@ -51,7 +48,10 @@ def task_1(count_of_companies: int):
 
 if __name__ == '__main__':
     user = int(input('Введите количество компаний\n>>> '))
-    result = task_1(user)
-    print(f'Средний доход компаний за год - {result[0]}\n'
-          f'Компании с доходом выше среднего - {result[1]}\n'
-          f'Компании с доходом ниже среднего - {result[2]}\n')
+    avg, comp_above, comp_below = task_1(user)
+    names_comp_above = [comp.name for comp in comp_above]
+    names_comp_below = [comp.name for comp in comp_below]
+
+    print(f'Средний доход компаний за год - {avg}\n'
+          f'Компании с доходом выше среднего - {names_comp_above}\n'
+          f'Компании с доходом ниже среднего - {names_comp_below}\n')
