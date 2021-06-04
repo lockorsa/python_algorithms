085a61b177d5399b7561b4d2c0a09d0276b4127b
lockorsa <lockorsa@yandex.ru>
2021-06-04 23:11:20 +0700

    Tasks done

--- Lesson_7/Task_2.py
+++ Lesson_7/Task_2.py
@@ -4,23 +4,42 @@
 
 Выведите на экран исходный и отсортированный массивы.
 """
-from random import randint
+from random import uniform
 
 
 def make_array():
     size = 20
-    return [randint(1, 50) for _ in range(size)]
+    return [uniform(0, 50).__round__(2) for _ in range(size)]
 
 
-def merge_sort(arr):
-    middle = len(arr) // 2  # + 1 if len(arr) % 2 == 1 else len(arr) // 2
-    left, right = arr[:middle], arr[:middle]
+def merge(left, right):
+    result = []
+    left_index, right_index = 0, 0
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
 
-    return middle
+    result += left[left_index:]
+    result += right[right_index:]
+    return result
+
+
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+
+    middle = len(array) // 2
+    left = merge_sort(array[:middle])
+    right = merge_sort(array[middle:])
+
+    return merge(left, right)
 
 
 if __name__ == '__main__':
     a = make_array()
     print(a)
-    print(a[:10], a[10:])
     print(merge_sort(a))
